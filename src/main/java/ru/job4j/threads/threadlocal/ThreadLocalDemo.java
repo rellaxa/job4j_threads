d238b33479af37af4c0e84ad8d88988a502fdeaa
relaxa <ewg.nadeev@yandex.ru>
2024-11-11 17:46:14 +0300

    9. ThreadLocal [#504912]

---
+++ src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,19 @@
+package ru.job4j.threads.threadlocal;
+
+import java.util.stream.IntStream;
+
+public class ThreadLocalDemo {
+
+    public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new FirstThread();
+        Thread second = new SecondThread();
+        threadLocal.set("Это поток main.");
+        System.out.println(threadLocal.get());
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+}
