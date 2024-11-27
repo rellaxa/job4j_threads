0d4e8c85b3c6a8949e235b40c85b627394227767
relaxa <ewg.nadeev@yandex.ru>
2024-11-28 01:34:47 +0300

    1. Реализовать шаблон Producer Consumer. [#1098 #285511]

---
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -0,0 +1,37 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    private final int size;
+
+    public SimpleBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() == size) {
+            wait();
+        }
+        queue.offer(value);
+        notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.isEmpty()) {
+            this.wait();
+        }
+        T el = queue.poll();
+        notifyAll();
+        return el;
+    }
+}
---
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -0,0 +1,76 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleBlockingQueueTest {
+
+    private Thread produce(SimpleBlockingQueue<Integer> queue, int size) {
+        return new Thread(() -> {
+            try {
+                for (int i = 0; i < size; i++) {
+                    queue.offer(i);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    private Thread consume(SimpleBlockingQueue<Integer> queue, int size) {
+        return new Thread(() -> {
+            try {
+                for (int i = 0; i < size; i++) {
+                    queue.poll();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    @Test
+    public void whenProducerAndConsumerCorrect() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = produce(queue, 5);
+        Thread consumer = consume(queue, 4);
+
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(queue.poll()).isEqualTo(4);
+    }
+
+    @Test
+    public void whenBlockProducer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+        Thread producer = produce(queue, 5);
+        Thread consumer = consume(queue, 4);
+
+        producer.start();
+        Thread.sleep(1000);
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(queue.poll()).isEqualTo(4);
+    }
+
+    @Test
+    public void whenBlockConsumer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+        Thread producer = produce(queue, 4);
+        Thread consumer = consume(queue, 3);
+
+        consumer.start();
+        Thread.sleep(1000);
+        producer.start();
+        consumer.join();
+        producer.join();
+        assertThat(queue.poll()).isEqualTo(3);
+    }
+}
